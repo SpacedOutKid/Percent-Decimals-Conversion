e97ab8ff7311d0a7b8d2025058710c1d7359477b
Armontae <56209424+SpacedOutKid@users.noreply.github.com>
2020-01-29 11:25:08 -0500

    Add files via upload

---
+++ Converting Percentages into Decimals/Converting Percentages into Decimals.py
@@ -0,0 +1,19 @@
+from tkinter import *
+master=Tk()
+master.title('Convert Percents & Decimals')
+
+
+
+
+'''startingPercent=eval(input("Enter your percentage if none enter 0: %"))
+if startingPercent>0:
+    endingDecimal=(startingPercent/100)
+    print(f'Your percent shown as a decimal is {endingDecimal} \n')    
+else:
+    print("No percent to convert \n\n")
+startingDecimal=eval(input("Enter the decimal you want to convert if none ener 0: "))
+if startingDecimal<100.0:
+    endingPercent=(startingDecimal*100)
+    print(f'Your decimal converted as a percent is %{endingPercent}')
+elif startingDecimal<=0:
+    print('No decimal to convert')'''
